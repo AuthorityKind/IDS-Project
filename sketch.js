fd25199be180c979003750085234de6b9f93c37b
AuthorityKind <Johannesduerlund@gmail.com>
2023-03-08 11:19:36 +0100

    maybe diff idk

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
-function setup() {
-    createCanvas(450, 400);
+var api = "https://spapi.dev/api/";
+var num = 1;
+var episodes = "episodes/";
+var locations = "locations/";
+var families = "families/";
+var characters = "characters/";
+
+var curEpisode;
+
+var curData;
+
+
+function preload() {
+  curEpisode = episodes + num;
+  curData = loadJSON(api+curEpisode);
+ 
 }
 
+function setup() {
+  createCanvas(400, 400); 
+  console.log(curData.data.name);
+} 
+
 function draw() {
-    background(220);
+  fill(200,200,200);
+  rect(100,100,100,100);
 }
