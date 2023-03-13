8ce2efd0fd7f796cc78ac9459aa5ac866c569938
AuthorityKind <Johannesduerlund@gmail.com>
2023-03-13 17:37:44 +0100

    shadow wizard money gang

--- sketch.js
+++ sketch.js
@@ -7,7 +7,15 @@ var resource = {
 }
 var num = 1;
 
-var gerald = {
+var lmao = 0;
+
+var Characters = {
+  //gerald: Object
+}
+
+//var names;
+
+var characterPreset = {
   id: null,
   name: null,
   age: null,
@@ -18,27 +26,53 @@ var gerald = {
 }
 
 function preload() {
-  loadJSON(api + resource.characters + num,loadCharacter)
+
+  loadJSON(api + "characters", loadTotal);
+  //loadJSON(api + resource.characters + num, loadCharacter)
 }
 
 function setup() {
   createCanvas(400, 400);
-  printCharacter(gerald);
+  console.log(lmao);
+  //initializeCharacter();
+  //printCharacter(Characters.gerald);
 }
 
 function loadCharacter(json) {
-  var keyArr = Object.keys(gerald);
+  var keyArr = Object.keys(Characters.gerald);
   for (var i = 0; i < keyArr.length; i++) {
-    gerald[keyArr[i]] = json.data[keyArr[i]];
+    Characters.gerald[keyArr[i]] = json.data[keyArr[i]];
   }
 }
 
 function printCharacter() {
-  var keyArr = Object.keys(gerald);
-  var valueArr = Object.values(gerald);
+  var keyArr = Object.keys(Characters.gerald);
+  var valueArr = Object.values(Characters.gerald);
 
   console.log("Here are Gerald's traits");
   for (var i = 0; i < keyArr.length; i++) {
     console.log(keyArr[i] + ": " + valueArr[i]);
   }
+}
+
+//needs to be paramitized
+function initializeCharacter() {
+  Characters.gerald = characterPreset;
+}
+
+function loadTotal(json) {
+  var total = "total";
+
+  lmao = Number(json.meta[total]);
+
+
+
+  //console.log(lmao);
+  // var names = new Array(json.length);
+
+  // for(var i = 0; i < names.length; i++){
+  //   names[i] = json[i].data.name;
+
+  //   console.log(names[i]);
+  // }
 }
