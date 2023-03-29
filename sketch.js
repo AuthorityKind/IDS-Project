f9bccf2649587e34122a5eac39ae074bda5a1f48
AuthorityKind <54275464+AuthorityKind@users.noreply.github.com>
2023-03-29 12:13:32 +0200

    javascript is my mortal enemy

--- sketch.js
+++ sketch.js
@@ -5,23 +5,20 @@ var resource = {
   families: "families/",
   characters: "characters/",
 };
-var num = 1;
 
-var total = 0;
-var test;
-var characters = {};
+var characters = {
+  id: [],
+  name: [],
+  age: [],
+  sex: [],
+  occupation: [],
+  religion: [],
+  family: [],
+}
 
-var characterPreset = {
-  id: null,
-  name: null,
-  age: null,
-  sex: null,
-  occupation: null,
-  religion: null,
-  family: null,
-};
+var total = 200;
 
-var names = [];
+var charactersKeys = Object.keys(characters);
 
 function preload() {
   loadJSON(api + "characters/", loadData);
@@ -29,43 +26,40 @@ function preload() {
 
 function setup() {
   //createCanvas(400, 400);
-  //console.log(test);
-  console.log(names);
-}
-/*
-function loadCharacter(json) {
-  var keyArr = Object.keys(Characters.gerald);
-  for (var i = 0; i < keyArr.length; i++) {
-    Characters.gerald[keyArr[i]] = json.data[keyArr[i]];
-  }
+
+  printCharacter(189);
 }
 
-function printCharacter() {
-  var keyArr = Object.keys(Characters.gerald);
-  var valueArr = Object.values(Characters.gerald);
+function getCharacter(index) {
+  var out = Array(charactersKeys.length);
 
-  console.log("Here are Gerald's traits");
-  for (var i = 0; i < keyArr.length; i++) {
-    console.log(keyArr[i] + ": " + valueArr[i]);
+  for (var i = 0; i < out.length; i++) {
+    out[i] = characters[charactersKeys[i]][index];
   }
+
+  return out;
 }
-function initializeCharacter() {
-  Characters.gerald = characterPreset;
+
+function printCharacter(index) {
+  var out = Array(charactersKeys.length);
+
+  for (var i = 0; i < out.length; i++) {
+    out[i] = characters[charactersKeys[i]][index];
+  }
+
+  console.log(out);
 }
-*/
 
 function loadData(json) {
-  total = Number(json.meta["total"]);
-  names = new Array(total);
-  test = json.data[0]["name"];
-
-  for (var i = 0; i < Number(json.meta["last_page"]); i++) {
-    loadJSON(api + "characters?page=" + i++,loadDataCluster)
+  for (var i = 1; i < Number(json.meta["last_page"]) - 2; i++) {
+    loadJSON(api + "characters?page=" + i, loadDataCluster)
   }
 }
 
 function loadDataCluster(json) {
   for (var i = 0; i < Number(json.meta["per_page"]); i++) {
-    names[i + (Number(json.meta["from"]) - 1)] = json.data[i]["name"];
+    for (var j = 0; j < charactersKeys.length; j++) {
+      characters[charactersKeys[j]][i + (Number(json.meta["from"]) - 1)] = json.data[i][charactersKeys[j]];
+    }
   }
 }
