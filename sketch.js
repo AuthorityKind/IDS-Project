6218af29bbc2cb1483687a114b2614f0b7c2e82d
AuthorityKind <54275464+AuthorityKind@users.noreply.github.com>
2023-04-12 12:59:19 +0200

    it's fucked, fix pls

--- sketch.js
+++ sketch.js
@@ -1,38 +1,101 @@
 var api = "https://spapi.dev/api/";
+var arr = [];
+var isLoading = false;
+var loadCheck = {
+  characters: false,
+  locations: false,
+  families: false,
+  episodes: false
+}
+const loadKeys = Object.keys(loadCheck);
 
 var characters;
 var charactersKeys = ["id", "name", "age", "sex", "occupation", "religion", "family"];
-var arr = [];
+
+var locations;
+var locationKeys = ["id", "name"];
+
+var families;
+var familiesKeys = ["id", "name", "characters"];
+
+var episodes;
+var episodeKeys = ["id", "name", "season", "episode", "air_date"];
 
 function preload() {
+  characters = new Data(api + "characters", charactersKeys);
+  locations = new Data(api + "locations", locationKeys);  
+  families = new Data(api + "families", familiesKeys);
+  episodes = new Data(api + "episodes", episodeKeys);
+  initiateLoading();
 }
 
 function setup() {
-  
-  var characterUrl = "https://spapi.dev/api/characters";
-  characters = new Data(characterUrl, charactersKeys);
-
-  loadJSON(characters.url, loadData);
-  characters.setContent(arr);
+  //loadJSON(characters.url, loadData);
   console.log(characters.content);
+  console.log(locations.content);
+  console.log(families.content);
+  console.log(episodes.content);
+
+  //arr = resetArr();
+  //loadJSON(locations.url, loadData)
+
+}
+
+function initiateLoading(){
+  for(var i = 0; i < loadKeys.length; i++){
+    while(loadCheck[loadKeys[]] == false){
+      if(isLoading == false){
+        loadJSON(getLoadingCollection().url, loadData);
+      }
+    }
+  }
+  loadKeys.forEach(key => {
+    
+  });
 }
 
 function loadData(json) {
-  const realTotal = Number(json.meta["total"]);
+  //const realTotal = Number(json.meta["last_page"]);
+  isLoading = true;
   const fakeTotal = 20;
 
   for (var i = 1; i <= fakeTotal; i++) {
-    loadJSON(api + "characters?page=" + i, loadDataCluster);
+    loadJSON(api + checkCurrentlyLoading() + "?page=" + i, loadDataCluster);
   }
+
+  loadCheck[checkCurrentlyLoading()] = true;
+  isLoading = false;
 }
 
 function loadDataCluster(json) {
-  const indexCounter = Number(json.meta["from"]) - 1;
   for (var i = 0; i < Number(json.meta["per_page"]); i++) {
-    var obj = {};
-    for (var j = 0; j < charactersKeys.length; j++) {
-      obj[charactersKeys[j]] = json.data[i][charactersKeys[j]];
+    const obj = json.data[i];
+    getLoadingCollection().content.push(obj);
+  }
+}
+
+function resetArr() {
+  const out = [];
+  return out;
+}
+
+function checkCurrentlyLoading(){
+  for(var i = 0; i < loadKeys.length; i++){
+    if(loadCheck[loadKeys[i]] == false){
+      return loadKeys[i];
     }
-    arr.push(obj);
+  }
+}
+
+function getLoadingCollection(){
+  switch (checkCurrentlyLoading()){
+    case loadKeys[0]:
+      return characters;
+    case loadKeys[1]:
+      return locations;
+    case loadKeys[2]:break;
+      return families;
+    case loadKeys[3]:
+      return episodes;
   }
 }
